f526d4ff831fa3c61533f79b3c0950aeea7f747d
Thiago Pereira <titiura@gmail.com>
2023-04-05 16:12:00 -0300

    fix: refatorando login.test aspas simples

--- front-end/src/tests/Login.test.js
+++ front-end/src/tests/Login.test.js
@@ -1,136 +1,144 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
-import renderWithRouter  from './helpers/RenderWithRouter'
-import Login from '../pages/Login';
+import React from "react";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import renderWithRouter from "./helpers/RenderWithRouter";
+import Login from "../pages/Login";
 
-describe('Tela de Login', () => {
-  it('Verifica se a tela de Login e renderiza corretamente ', () => {
+describe("Tela de Login", () => {
+  it("Verifica se a tela de Login e renderiza corretamente ", () => {
     renderWithRouter(<App />);
 
-    expect(screen.getByRole('heading', { name: /nome app/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /nome app/i })
+    ).toBeInTheDocument();
   });
-  it('Verifica se existe input de Login', () => {
+  it("Verifica se existe input de Login", () => {
     renderWithRouter(<App />);
 
-    expect(screen.getByTestId('common_login__input-email')).toBeInTheDocument();
+    expect(screen.getByTestId("common_login__input-email")).toBeInTheDocument();
   });
-  it('Verifica se existe input de senha', () => {
+  it("Verifica se existe input de senha", () => {
     renderWithRouter(<App />);
 
-    expect(screen.getByTestId('common_login__input-password')).toBeInTheDocument();
+    expect(
+      screen.getByTestId("common_login__input-password")
+    ).toBeInTheDocument();
   });
-  it('verifica se existe um botão de Login', () => {
-    renderWithRouter(<App />);
-    
-    expect(screen.getByTestId('common_login__button-login')).toBeInTheDocument();
-  });
-
-  it('verifica se existe um botão de criação de conta', () => {
+  it("verifica se existe um botão de Login", () => {
     renderWithRouter(<App />);
 
-    expect(screen.getByTestId('common_login__button-register')).toBeInTheDocument();
+    expect(
+      screen.getByTestId("common_login__button-login")
+    ).toBeInTheDocument();
   });
 
-  it('Verifica se button esta desabilitado ao entrar na tela', () => {
+  it("verifica se existe um botão de criação de conta", () => {
     renderWithRouter(<App />);
 
-    const button = screen.getByRole('button', { name: /login/i });
+    expect(
+      screen.getByTestId("common_login__button-register")
+    ).toBeInTheDocument();
+  });
+
+  it("Verifica se button esta desabilitado ao entrar na tela", () => {
+    renderWithRouter(<App />);
+
+    const button = screen.getByRole("button", { name: /login/i });
     expect(button).toBeDisabled();
   });
 
-  it('Verifica se o botão e habilitado com email e senhas validos. ', () => {
+  it("Verifica se o botão e habilitado com email e senhas validos. ", () => {
     renderWithRouter(<App />);
 
-    const email = 'my@email.com';
-    const password = '123456';
+    const email = "my@email.com";
+    const password = "123456";
 
-    const btnTest = screen.getByRole('button', { name: /login/i });
-    const testEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
+    const btnTest = screen.getByRole("button", { name: /login/i });
+    const testEmail = screen.getByTestId("common_login__input-email");
+    const inputPassword = screen.getByTestId("common_login__input-password");
 
     userEvent.type(testEmail, email);
     userEvent.type(inputPassword, password);
     expect(btnTest).not.toBeDisabled();
   });
 
-  it('Verifica se o botão de Cadastro redireciona pra a tela de criação de conta', () => {
+  it("Verifica se o botão de Cadastro redireciona pra a tela de criação de conta", () => {
     const { history } = renderWithRouter(<App />);
 
-    const btnCreateAccount = screen.getByTestId('common_login__button-register');
+    const btnCreateAccount = screen.getByTestId(
+      "common_login__button-register"
+    );
     userEvent.click(btnCreateAccount);
 
-    expect(history.location.pathname).toBe('/register');
+    expect(history.location.pathname).toBe("/register");
   });
 
-  it('Verifica se colocado email e senha validos mais sem cadastro no banco de dados retorna mensagem de erro', () => {
+  it("Verifica se colocado email e senha validos mais sem cadastro no banco de dados retorna mensagem de erro", () => {
     renderWithRouter(<App />);
 
-    const email = 'my@email.com';
-    const password = '123456';
+    const email = "my@email.com";
+    const password = "123456";
 
-    const btnLogin = screen.getByRole('button', { name: /login/i });
-    const testEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
+    const btnLogin = screen.getByRole("button", { name: /login/i });
+    const testEmail = screen.getByTestId("common_login__input-email");
+    const inputPassword = screen.getByTestId("common_login__input-password");
 
     userEvent.type(testEmail, email);
     userEvent.type(inputPassword, password);
     userEvent.click(btnLogin);
 
     expect(screen.getByText(/erro/i)).toBeInTheDocument();
-
   });
 
-  it('Verifica se colocado email e senha validos cadastrados no DB redireciona para tela de produtos', () => {
-    const { history } = renderWithRouter(<Login />, '/customer/products');
+  it("Verifica se colocado email e senha validos cadastrados no DB redireciona para tela de produtos", () => {
+    const { history } = renderWithRouter(<Login />, "/customer/products");
 
-    const emailValid = 'zebirita@email.com';
-    const passwordValid = '$#zebirita#$';
+    const emailValid = "zebirita@email.com";
+    const passwordValid = "$#zebirita#$";
 
-    const btnLogin = screen.getByRole('button', { name: /login/i });
-    const testEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
+    const btnLogin = screen.getByRole("button", { name: /login/i });
+    const testEmail = screen.getByTestId("common_login__input-email");
+    const inputPassword = screen.getByTestId("common_login__input-password");
 
     userEvent.type(testEmail, emailValid);
     userEvent.type(inputPassword, passwordValid);
     userEvent.click(btnLogin);
 
-    expect(history.location.pathname).toBe('/customer/products');
+    expect(history.location.pathname).toBe("/customer/products");
   });
 
-  it('Verifica se colocado email e senha de pessoa administradora , redireciona para tela de administrador', () => {
-    const { history } = renderWithRouter(<Login />, '/admin/manage');
+  it("Verifica se colocado email e senha de pessoa administradora , redireciona para tela de administrador", () => {
+    const { history } = renderWithRouter(<Login />, "/admin/manage");
 
-    const emailValid = 'adm@deliveryapp.com';
-    const passwordValid = '--adm2@21!!--';
+    const emailValid = "adm@deliveryapp.com";
+    const passwordValid = "--adm2@21!!--";
 
-    const btnLogin = screen.getByRole('button', { name: /login/i });
-    const testEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
+    const btnLogin = screen.getByRole("button", { name: /login/i });
+    const testEmail = screen.getByTestId("common_login__input-email");
+    const inputPassword = screen.getByTestId("common_login__input-password");
 
     userEvent.type(testEmail, emailValid);
     userEvent.type(inputPassword, passwordValid);
     userEvent.click(btnLogin);
 
-    expect(history.location.pathname).toBe('/admin/manage');
+    expect(history.location.pathname).toBe("/admin/manage");
   });
 
-  it('Verifica se colocado email e senha de pessoa vendedora , redireciona para tela de vendedor', () => {
-    const { history } = renderWithRouter(<Login />, '/seller/orders');
+  it("Verifica se colocado email e senha de pessoa vendedora , redireciona para tela de vendedor", () => {
+    const { history } = renderWithRouter(<Login />, "/seller/orders");
 
-    const emailValid = 'fulana@deliveryapp.com';
-    const passwordValid = 'fulana@123';
+    const emailValid = "fulana@deliveryapp.com";
+    const passwordValid = "fulana@123";
 
-    const btnLogin = screen.getByRole('button', { name: /login/i });
-    const testEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
+    const btnLogin = screen.getByRole("button", { name: /login/i });
+    const testEmail = screen.getByTestId("common_login__input-email");
+    const inputPassword = screen.getByTestId("common_login__input-password");
 
     userEvent.type(testEmail, emailValid);
     userEvent.type(inputPassword, passwordValid);
     userEvent.click(btnLogin);
 
-    expect(history.location.pathname).toBe('/seller/orders');
+    expect(history.location.pathname).toBe("/seller/orders");
   });
-
 });
