001a15cdc0d4d6e7a4b7b7dac1a53b1a09b2cf8f
Thiago Pereira <titiura@gmail.com>
2023-04-05 16:12:28 -0300

    feat: criando arquivos api - com axios

---
+++ front-end/src/services/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001/',
+});
+
+export const requestLogin = async (body) => {
+  const { data } = await api.post('/login', body);
+  return data;
+};
+
+export const requestRegister = async (body) => {
+  const { data } = await api.post('/register', body);
+  return data;
+};
+
+export default api;
